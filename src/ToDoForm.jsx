11be1f4c3405e318131046b685015184b4b06c7a
Vova <uladzimirfilipau@gmail.com>
2022-10-11 16:26:57 +0300

    feat: ToDoForm
    - add ToDoForm component
    - add ToDoForm styles
    - add const list
    - add const inpuValue
    - add func handleChange
    - add func handleSubmit

---
+++ src/ToDoForm.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './ToDoForm.css';
+
+function ToDoForm({ inputValue, handleChange, handleSubmit }) {
+  return (
+    <form className="form" onSubmit={handleSubmit}>
+      <input
+        className="form__input"
+        value={inputValue}
+        name="todo"
+        type="text"
+        onChange={handleChange}
+        placeholder="Enter todo..."
+        minLength="1"
+        maxLength="20"
+        required
+      />
+      <button className="form__button" type="submit" aria-label="Add todo">
+        Add
+      </button>
+    </form>
+  );
+}
+
+export default ToDoForm;
